43e79907d55bf76376daa7decc65a73edb2be0c0
Adam Coddington <me@adamcoddington.net>
2021-10-12 22:26:27 -0700

    Do not use vault adapter methods directly for read/write.

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,46 @@
-import { Plugin, parseYaml } from 'obsidian';
+import { Plugin, parseYaml, TFile } from "obsidian";
 
-import { getFilteredCsvData } from './csv_table';
-import { TableRenderer, renderErrorPre } from './render'
-import { CsvTableSpec } from './types';
+import { getFilteredCsvData } from "./csv_table";
+import { TableRenderer, renderErrorPre } from "./render";
+import { CsvTableSpec } from "./types";
 
 export default class CsvTablePlugin extends Plugin {
   async onload() {
-    this.registerMarkdownCodeBlockProcessor("csvtable", async (csvSpecString: string, el, ctx) => {
-      try {
-        let tableSpec: CsvTableSpec = {
-          source: ''  // Assert that this has a proper value below
-        }
+    this.registerMarkdownCodeBlockProcessor(
+      "csvtable",
+      async (csvSpecString: string, el, ctx) => {
         try {
-          tableSpec = parseYaml(csvSpecString)
+          let tableSpec: CsvTableSpec = {
+            source: "", // Assert that this has a proper value below
+          };
+          try {
+            tableSpec = parseYaml(csvSpecString);
+          } catch (e) {
+            throw new Error(`Could not parse CSV table spec: ${e.message}`);
+          }
+
+          if (!tableSpec.source) {
+            throw new Error("Parameter 'source' is required.");
+          }
+
+          const file = this.app.vault.getAbstractFileByPath(tableSpec.source);
+          if (!(file instanceof TFile)) {
+            throw new Error(
+              `CSV file '${tableSpec.source}' could not be found.`
+            );
+          }
+          const csvData = await this.app.vault.cachedRead(file);
+          console.log("Cached read success");
+
+          const filteredCsvData = getFilteredCsvData(tableSpec, csvData);
+          ctx.addChild(
+            new TableRenderer(filteredCsvData.columns, filteredCsvData.rows, el)
+          );
         } catch (e) {
-          throw new Error(`Could not parse CSV table spec: ${e.message}`)
+          renderErrorPre(el, e.message);
+          return;
         }
-
-        if (!tableSpec.source) {
-          throw new Error("Parameter 'source' is required.")
-        }
-
-        const exists = await this.app.vault.adapter.exists(tableSpec.source)
-        if (!exists) {
-          throw new Error(`CSV file '${tableSpec.source}' could not be found.`)
-        }
-        const csvData = await this.app.vault.adapter.read(tableSpec.source)
-
-        const filteredCsvData = getFilteredCsvData(tableSpec, csvData)
-        ctx.addChild(new TableRenderer(filteredCsvData.columns, filteredCsvData.rows, el));
-      } catch (e) {
-        renderErrorPre(el, e.message)
-        return
       }
-    });
+    );
   }
 }
